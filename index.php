b8c6ec11d591430ec0916f23e18a2bebde9db2a2
Naufal Geraldi <naufalgeraldi@gmail.com>
2023-03-26 02:10:50 +0700

    Applied new bootstrap

--- index.php
+++ index.php
@@ -1,19 +1,38 @@
 <?php require 'template/header.php'; ?>
 
+
 <div class="container">
     <div class="row">
         <h1>This is Homepage</h1>
     </div>
 
-    <div class="m-5"><hr></div>
-    <div class="m-5"><hr></div>
-    <div class="m-5"><hr></div>
-    <div class="m-5"><hr></div>
-    <div class="m-5"><hr></div>
-    <div class="m-5"><hr></div>
-    <div class="m-5"><hr></div>
-    <div class="m-5"><hr></div>
-    <div class="m-5"><hr></div>
+    <div class="m-5">
+        <hr>
+    </div>
+    <div class="m-5">
+        <hr>
+    </div>
+    <div class="m-5">
+        <hr>
+    </div>
+    <div class="m-5">
+        <hr>
+    </div>
+    <div class="m-5">
+        <hr>
+    </div>
+    <div class="m-5">
+        <hr>
+    </div>
+    <div class="m-5">
+        <hr>
+    </div>
+    <div class="m-5">
+        <hr>
+    </div>
+    <div class="m-5">
+        <hr>
+    </div>
 
 </div>
 
--- template/header.php
+++ template/header.php
@@ -31,12 +31,12 @@
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="icon" type="image/svg+xml" href="img/leaves.png">
 
     <link href="css/style_tmp2.css" rel="stylesheet">
     <script src="js/script.js"></script>
-
+    
 </head>
 
 <body>
@@ -69,5 +69,4 @@
     </nav>
 
     <div class="mb-4"></div>
-
     <?php require 'template/function.php'; ?>
