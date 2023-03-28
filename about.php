93aa09fe3ebc19b9f05622ad62d1603c47683897
Naufal Geraldi <naufalgeraldi@gmail.com>
2023-03-29 00:06:16 +0700

    About fix done deal
    issues copyright ngapung

--- about.php
+++ about.php
@@ -2,25 +2,32 @@
 
 <div class="container-md">
     <div class="row">
-        <h1>Yellow Table</h1>
-        <h1>Creative.</h1>
-        <br>
-        <div class="col-sm">
-            <p>Agensi yang berfokus pada Social Media Management, Content Production, dan Campaign Strategy. Kami adalah tim profesional yang berdedikasi untuk membantu bisnis Anda meningkatkan kehadiran online melalui media sosial dengan strategi yang efektif dan konten yang menarik.</p>
-        </div>
-        <div class="col-sm">
-            <p>Kami memahami bahwa setiap bisnis memiliki kebutuhan yang unik. Oleh karena itu, kami menawarkan layanan yang dapat disesuaikan dengan kebutuhan Anda. Tim kami akan bekerja sama dengan Anda untuk memahami tujuan Anda dan menciptakan strategi yang sesuai untuk mencapai tujuan tersebut.</p>
-        </div>
+        <h1 style="color: #fcb212;">Yellow Table</h1>
+        <h1 class="bawah">Creative <span style="color: #fcb212; margin-left: -20px;"">.</span></h1>
+        <div class=" bawah">
+                <hr>
     </div>
-    <br>
-    <div class="row">
-        <h2>We help brand find their identity</h2>
-        <br>
-        <div class="col-sm">
-            <p>Jangan ragu untuk menghubungi kami untuk konsultasi gratis dan temukan bagaimana kami dapat membantu meningkatkan kehadiran online bisnis Anda melalui social media management dan pembuatan konten yang efektif.</p>
-        </div>
+    <div class="col-sm">
+        <p>Agensi yang berfokus pada Social Media Management, Content Production, dan Campaign Strategy. Kami adalah tim profesional yang berdedikasi untuk membantu bisnis Anda meningkatkan kehadiran online melalui media sosial dengan strategi yang efektif dan konten yang menarik.</p>
     </div>
+    <div class="col-sm">
+        <p>Kami memahami bahwa setiap bisnis memiliki kebutuhan yang unik. Oleh karena itu, kami menawarkan layanan yang dapat disesuaikan dengan kebutuhan Anda. Tim kami akan bekerja sama dengan Anda untuk memahami tujuan Anda dan menciptakan strategi yang sesuai untuk mencapai tujuan tersebut.</p>
+    </div>
+</div>
+<br>
+<div class="row">
+    <h2>We help brand find their identity</h2>
     <br>
-<button type="button" class="btn btn-warning">Get In Touch!</button>
+    <div class="col-sm">
+        <p>Jangan ragu untuk menghubungi kami untuk konsultasi gratis dan temukan bagaimana kami dapat membantu meningkatkan kehadiran online bisnis Anda melalui social media management dan pembuatan konten yang efektif.</p>
+    </div>
+</div>
+<br>
+<a href="https://wa.me/62882000016684?text=Halo,%20Saya%20ingin%20konsultasi.">
+    <button type="button" class="btn-about">
+        Get In Touch
+    </button>
+</a>
 
-    <?php require 'template/footer.php'; ?>
+
+<?php require 'template/footer.php'; ?>
--- template/header.php
+++ template/header.php
@@ -4,7 +4,7 @@
 <head>
     <?php
     $pagename = '';
-    $websiteName = ' - Website';
+    $websiteName = ' - Yellow Table';
 
     $currentPage = basename($_SERVER['SCRIPT_NAME'], '.php');
 
@@ -32,7 +32,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    <link rel="icon" type="image/svg+xml" href="img/leaves.png">
+    <link rel="icon" type="image/svg+xml" href="img/Asset 2@2x.png">
 
     <link href="css/style.css" rel="stylesheet">
     <script src="js/script.js"></script>
