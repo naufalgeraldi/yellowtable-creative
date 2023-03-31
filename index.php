cdcbf4be4384fffb710981d140f648f325df3da8
Hafizh F. Yuspriana <hafizhfy@upi.edu>
2023-04-01 00:09:51 +0700

    Landing page

--- index.php
+++ index.php
@@ -52,15 +52,15 @@
         <div class="col ps-3 service-bg" style="background-image: url('svg/service-2.svg'); background-size: 85%;">
             <h2 style="margin-top: -30px;">Photography<br>Videography</h2>
             <p class="bottom-50" style="max-width: 260px; margin-left: 40px;">
-            <h4>Kami menyediakan layanan yang dapat disesuaikan dengan kebutuhan bisnis Anda, termasuk produksi video iklan, foto produk, fotografi branding, dan lainnya. Guna 
-            memahami dan membantu Anda mengembangkan konten visual yang konsisten sesuai dengan pesan brand Anda.</h4>
+            <h4>Kami menyediakan layanan yang dapat disesuaikan dengan kebutuhan bisnis Anda, termasuk produksi video iklan, foto produk, fotografi branding, dan lainnya. Guna
+                memahami dan membantu Anda mengembangkan konten visual yang konsisten sesuai dengan pesan brand Anda.</h4>
             </p>
         </div>
         <div class="col ps-3 service-bg" style="background-image: url('svg/service-3.svg');">
             <h2 style="margin-top: -30px;">Visual<br>Design</h2>
             <p class="bottom-50" style="max-width: 260px; margin-left: 40px;">
-            <h4> Membantu Anda dengan kebutuhan desain visual yang beragam, mulai dari logo, kartu nama, brosur, hingga konten pada social media. 
-            Kami berkomitmen untuk memahami merek Anda dan menciptakan desain yang konsisten dengan pesan dan nilai brand Anda.</h4>
+            <h4> Membantu Anda dengan kebutuhan desain visual yang beragam, mulai dari logo, kartu nama, brosur, hingga konten pada social media.
+                Kami berkomitmen untuk memahami merek Anda dan menciptakan desain yang konsisten dengan pesan dan nilai brand Anda.</h4>
             </p>
         </div>
     </div>
@@ -89,9 +89,61 @@
         </div>
     </div>
 
+    <div class="row row-cols-1 row-cols-md-3 g-4 bottom-50">
+        <div class="col">
+            <div class="card h-100">
+                <img src="img/3.png" class="card-img-top img-preview" alt="...">
+                <div class="card-img-overlay no-padding">
+                    <img src="svg/client_logo_bg.svg">
+                </div>
+                <div class="card-body no-h-padding">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="img/5.png" class="card-img-top" alt="...">
+                <div class="card-img-overlay no-padding">
+                    <img src="svg/client_logo_bg.svg">
+                </div>
+                <div class="card-body no-h-padding">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a short card.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="img/20.png" class="card-img-top" alt="...">
+                <div class="card-img-overlay no-padding">
+                    <img src="svg/client_logo_bg.svg">
+                </div>
+                <div class="card-body no-h-padding">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bottom-100">
+        <a href="#">
+            <button class="btn" style="margin-left: -10px;">
+                <span class="btn__inner">
+                    <span class="btn__slide"></span>
+                    <span class="btn__content">
+                        <span class="btn-txt">Discover More</span>
+                    </span>
+                </span>
+            </button>
+        </a>
+    </div>
+
     <!-- OUR CLIENTS -->
 
-    <div class="row bottom-100">
+    <div class="row bottom-50">
         <div class="col">
             <h1>Our <span class="ylw-txt">Clients.</span></h1>
         </div>
@@ -100,7 +152,67 @@
         </div>
     </div>
 
+    <div class="row bottom-100">
+        <div class="col align-self-center" style="text-align: center;">
+            <img src="img/reisen_logo.png" class="logo-thumbnail bottom-25" alt="Reisen">
+            <h3 class="ylw-bg-txt mx-auto">Reisen Gear</h3>
+        </div>
+        <div class="col align-self-center" style="text-align: center;">
+            <img src="img/reisen_logo.png" class="logo-thumbnail bottom-25" alt="Reisen">
+            <h3 class="ylw-bg-txt mx-auto">Reisen Gear</h3>
+        </div>
+        <div class="col align-self-center" style="text-align: center;">
+            <img src="img/reisen_logo.png" class="logo-thumbnail bottom-25" alt="Reisen">
+            <h3 class="ylw-bg-txt mx-auto">Reisen Gear</h3>
+        </div>
+        <div class="col align-self-center" style="text-align: center;">
+            <img src="img/reisen_logo.png" class="logo-thumbnail bottom-25" alt="Reisen">
+            <h3 class="ylw-bg-txt mx-auto">Reisen Gear</h3>
+        </div>
+        <div class="col align-self-center" style="text-align: center;">
+            <img src="img/reisen_logo.png" class="logo-thumbnail bottom-25" alt="Reisen">
+            <h3 class="ylw-bg-txt mx-auto">Reisen Gear</h3>
+        </div>
+        <div class="col align-self-center" style="text-align: center;">
+            <img src="img/reisen_logo.png" class="logo-thumbnail bottom-25" alt="Reisen">
+            <h3 class="ylw-bg-txt mx-auto">Reisen Gear</h3>
+        </div>
+    </div>
 
+
+</div>
+
+<div class="ylw-bg text-center" style="padding: 128px 0px">
+    <h1 class="tight-line-height mb-5 bottom-50">
+        Interested?<br><span class="white-txt">Contact us</span> right away!
+    </h1>
+    <div class="row" style="padding: 0 30vw; --bs-gutter-x: 0;">
+        <div class="col">
+            <a href="https://wa.me/62882000016684?text=Halo,%20Saya%20ingin%20konsultasi.">
+                <button style="border: 0; padding: 20px 40px 12px 40px; width: 290px;" class="contact-btn">
+                    <span style="display:inline-flex; top: 10px;">
+                        <img src="img/reisen_logo.png" alt="wa" style="height:30px; margin-right:10px;">
+                        <span>
+                            <h3 style=" margin-bottom: 0; margin-top: 1px;">Send Whatsapp</h3>
+                        </span>
+                    </span>
+                </button>
+            </a>
+        </div>
+        <div class="col">
+            <!-- BIAR KIRIM EMAIL: <a href="mailto:EMAILADDRESS"></a> -->
+            <a href="#">
+                <button style="border: 0; padding: 20px 40px 12px 40px; width: 290px;" class="contact-btn contact-ouline-btn">
+                    <span style="display:inline-flex; top: 10px;">
+                        <img src="img/reisen_logo.png" alt="wa" style="height:30px; margin-right:10px;">
+                        <span>
+                            <h3 style=" margin-bottom: 0; margin-top: 1px;">Send Email</h3>
+                        </span>
+                    </span>
+                </button>
+            </a>
+        </div>
+    </div>
 </div>
 
 
